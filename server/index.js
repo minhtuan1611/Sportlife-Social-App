0058119f50d383072f416e71507446737a41bda1
minhtuan1611 <tuan.mn51@gmail.com>
2024-09-21 11:42:57 +0300

    chore: initialize frontend

--- server/index.js
+++ server/index.js
@@ -42,8 +42,6 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage })
 
-// Routes with files
-
 app.post('/auth/register', upload.single('picture'), register)
 app.post('/posts', verifyToken, upload.single('picture'), createPost)
 
