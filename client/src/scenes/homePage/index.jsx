88a32f0d1be70b47d960fefd6e08ce6a43651a43
minhtuan1611 <tuan.mn51@gmail.com>
2024-10-12 13:12:18 +0300

    feat: Widgets

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,10 @@ import { Box, useMediaQuery } from '@mui/material'
 import { useSelector } from 'react-redux'
 import Navbar from 'scenes/navbar'
 import UserWidget from 'scenes/widgets/UserWidget'
+import MyPostWidget from 'scenes/widgets/MyPostWidget'
+import PostsWidget from 'scenes/widgets/PostsWidget'
+import AdvertWidget from 'scenes/widgets/AdvertWidget'
+import FriendListWidget from 'scenes/widgets/FriendListWidget'
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)')
@@ -23,10 +27,15 @@ const HomePage = () => {
         <Box
           flexBasis={isNonMobileScreens ? '42%' : undefined}
           mt={isNonMobileScreens ? undefined : '2rem'}
-        ></Box>
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
+            <AdvertWidget />
             <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
